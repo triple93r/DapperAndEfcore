4954e8a6fb659809e9db278719f4062713a8cf9a
triple93r <56282248+triple93r@users.noreply.github.com>
2023-12-08 22:16:03 +0530

    init22

--- DapperAndEfcore/Controllers/HomeController.cs
+++ DapperAndEfcore/Controllers/HomeController.cs
@@ -8,7 +8,7 @@ using System.Diagnostics;
 
 namespace DapperAndEfcore.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : Controller, IDisposable
     {
         private readonly ILogger<HomeController> _logger;
         private readonly ApplicationDbContext _context;
@@ -30,29 +30,92 @@ namespace DapperAndEfcore.Controllers
         {
             return View();
         }
+
+        [HttpGet]
         public IActionResult getAll()
         {
             var c = _context.tbl_Subject.ToList();
             return View(c);
         }
 
+        [HttpGet]
+        public IActionResult AddEfCore()
+        {
+            return View();
+        }
 
+        [HttpPost]
+        public IActionResult AddEfCore(tbl_subject sbj)
+        {
+            _context.tbl_Subject.Add(sbj);
+            _context.SaveChanges();
+            return RedirectToAction("getAll");
+        }
+
+        [HttpGet]
         public IActionResult getAll2()
         {
             var DBCon2 = _configuration.GetSection("DBNames");
             using var conn2 = new SqlConnection(_configuration.GetConnectionString(DBCon2.Value));
-
-            var dbparams = new DynamicParameters();
-            dbparams.Add("@LoginId", username, DbType.String);
-            //dbparams.Add("@Pwd", UIHelper.Encrypt(Pwd),DbType.String);
-            dbparams.Add("@Pwd", password, DbType.String);
             conn2.Open();
-            var user = conn2.Query("sp_CheckLoginId", dbparams, commandType: CommandType.StoredProcedure).SingleOrDefault();
+            var m1m = conn2.Query<tbl_subject>("select * from tbl_Subject", commandType: CommandType.Text);
             conn2.Close();
+            //int res = conn.Execute(Sqlstr, dbparams, transaction);
+            return View(m1m.ToList());
+        }
 
+        [HttpGet]
+        public IActionResult AddSubject()
+        {
+            return View();
+        }
 
-            var c = _context.tbl_Subject.ToList();
-            return View(c);
+        [HttpPost]
+        public IActionResult AddSubject(tbl_subject sbj)
+        {
+            var DBCon2 = _configuration.GetSection("DBNames");
+            using var conn2 = new SqlConnection(_configuration.GetConnectionString(DBCon2.Value));
+            var dbparams = new DynamicParameters();
+            dbparams.Add("@sName", sbj.sName, DbType.String);
+            dbparams.Add("@Author", sbj.Author, DbType.String);
+            dbparams.Add("@Price", sbj.Price, DbType.Decimal);
+            dbparams.Add("@grade", sbj.grade, DbType.String);
+            conn2.Open();
+            //var Sqlstr = conn2.Query<tbl_subject>("insert into tbl_subject(sName,Author,Price,grade) values('"+sbj.sName+"','"+sbj.Author+"',"+sbj.Price+",'"+sbj.grade+"')", commandType: CommandType.Text);
+            var Sqlstr = "insert into tbl_subject(sName,Author,Price,grade) values(@sName, @Author, @Price, @grade); SELECT @@IDENTITY; ";
+            int res = conn2.QueryFirstOrDefault<int>(Sqlstr, dbparams);
+            conn2.Close();
+            
+            return RedirectToAction("getAll2");
+        }
+
+        [HttpGet]
+        public IActionResult EditSubject(int id)
+        {
+            var DBCon2 = _configuration.GetSection("DBNames");
+            using var conn2 = new SqlConnection(_configuration.GetConnectionString(DBCon2.Value));
+            conn2.Open();
+            var b = conn2.Query<tbl_subject>("select * from tbl_Subject where Id="+id, commandType: CommandType.Text).SingleOrDefault();
+            conn2.Close();
+            return View(b);
+        }
+        [HttpPost]
+        public IActionResult EditSubject(tbl_subject sbj)
+        {
+            var DBCon2 = _configuration.GetSection("DBNames");
+            using var conn2 = new SqlConnection(_configuration.GetConnectionString(DBCon2.Value));
+            var dbparams = new DynamicParameters();
+            dbparams.Add("@Id", sbj.Id, DbType.Int32);
+            dbparams.Add("@sName", sbj.sName, DbType.String);
+            dbparams.Add("@Author", sbj.Author, DbType.String);
+            dbparams.Add("@Price", sbj.Price, DbType.Decimal);
+            dbparams.Add("@grade", sbj.grade, DbType.String);
+            conn2.Open();
+            //conn2.Query<tbl_subject>("update tbl_Subject set sName=@sName, grade=@grade where Id=@Id", commandType: CommandType.Text);
+            var Sqlstr = "update tbl_Subject set sName=@sName, grade=@grade where Id=@Id;";
+            int res = conn2.QueryFirstOrDefault<int>(Sqlstr, dbparams);
+            conn2.Close();
+            return RedirectToAction("getAll2");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
--- DapperAndEfcore/obj/Debug/net6.0/DapperAndEfcore.AssemblyInfo.cs
+++ DapperAndEfcore/obj/Debug/net6.0/DapperAndEfcore.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DapperAndEfcore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4bc5d918fae2eba67d8fc22f525779a955d50a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("DapperAndEfcore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DapperAndEfcore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
